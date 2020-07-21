f7c531770eed6098913cc205429259caf1c3f2c4
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-21 01:06:29 -0400

    portfolo work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// IMAGES //
+
+import Distro from './images/distro.png';
+import Distro2 from './images/distro2.png';
+
+import DistroEJS from './images/distro-ejs.png';
+import DistroEJS2 from './images/distro-ejs2.png';
+
+import HAOly from './images/haoly.png';
+import HAOly2 from './images/haoly2.png';
+
+import RootStock from './images/rootstock.png';
+import RootStock2 from './images/rootstock2.png';
+
+import SATGen from './images/sat-gen.png';
+import SATGen2 from './images/sat-gen2.png';
+
+import Zach from './images/zach.jpg';
+
+class App extends React.Component {
+  state = {
+    display:'rootstock'
+  }
+
+  displayRoute = (e) => {
+    let routeToDisplay = e.target.getAttribute('routetodisplay');
+    this.setState({
+      display:routeToDisplay
+    })
+  }
+
+  render = () => {
+    return <div className="app-container">
+      <div className="display-container">
+        {this.state.display === 'zach' ? <img src={Zach}/> : null}
+
+        {this.state.display === 'distro' ?
+          <a target="_blank" href="https://distro-app.herokuapp.com/">
+            <img src={Distro}/>
+            <img src={Distro2}/>
+          </a> : null}
+
+        {this.state.display === 'sat-gen' ?
+          <a target="_blank" href="https://enigmatic-escarpment-97432.herokuapp.com/">
+            <img src={SATGen}/>
+            <img src={SATGen2}/>
+          </a> : null}
+
+        {this.state.display === 'rootstock' ?
+          <a target="_blank" href="http://rootstock.herokuapp.com/">
+            <img src={RootStock}/>
+            <img src={RootStock2}/>
+          </a> : null}
+
+        {this.state.display === 'distro-ejs' ?
+          <a target="_blank" href="https://distro-filmtv.herokuapp.com/">
+            <img src={DistroEJS}/>
+            <img src={DistroEJS2}/>
+          </a> : null}
+
+        {this.state.display === 'haoly' ?
+          <a target="_blank" href="https://zachherwitz.github.io/chineseapp/">
+            <img src={HAOly}/>
+            <img src={HAOly2}/>
+          </a> : null}
+
+      </div>
+      <div className="project-button-container">
+        <button onClick={this.displayRoute} routetodisplay="zach">ZACH</button>
+        <button onClick={this.displayRoute} routetodisplay="distro">DISTRO</button>
+        <button onClick={this.displayRoute} routetodisplay="rootstock">ROOTSTOCK</button>
+        <button onClick={this.displayRoute} routetodisplay="sat-gen">SAT-GEN</button>
+        <button onClick={this.displayRoute} routetodisplay="distro-ejs">DISTRO-EJS</button>
+        <button onClick={this.displayRoute} routetodisplay="haoly">HAOLY</button>
+      </div>
     </div>
-  );
+  }
 }
 
 export default App;
